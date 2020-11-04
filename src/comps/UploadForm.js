47c492a80d7a48773cb288551050484b4ec01313
redwan zia <redwan.zia@gmail.com>
2020-11-03 21:11:22 -0500

    progress bar component added with functionality

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
+import ProgressBar from './ProgressBar';
 
 const UploadForm = () => {
 	const [ file, setFile ] = useState(null);
 	const [ error, setError ] = useState(null);
 	const types = [ 'image/png', 'image/jpeg' ];
 
-	const changeHandler = (e) => {
+	const handleChange = (e) => {
 		const selected = e.target.files[0];
 		if (selected && types.includes(selected.type)) {
 			setFile(selected);
@@ -16,13 +17,17 @@ const UploadForm = () => {
 		}
 	};
 	return (
-		<div>
-			<input type='file' onChange={changeHandler} />
+		<form>
+			<label>
+				<input type='file' onChange={handleChange} />
+				<span>+</span>
+			</label>
 			<div className='output'>
 				{error && <div className='error'>{error}</div>}
 				{file && <div>{file.name}</div>}
+				{file && <ProgressBar file={file} setFile={setFile} />}
 			</div>
-		</div>
+		</form>
 	);
 };
 
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,21 +1,20 @@
-import * as firebase from 'firebase/app'
-import 'firebase/storage'
-import 'firebase/firestore'
-
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzAcmL971bApw0_yvAIXFrApEJA0gy3rY",
-  authDomain: "fir-photo-8d94a.firebaseapp.com",
-  databaseURL: "https://fir-photo-8d94a.firebaseio.com",
-  projectId: "fir-photo-8d94a",
-  storageBucket: "fir-photo-8d94a.appspot.com",
-  messagingSenderId: "312521110742",
-  appId: "1:312521110742:web:db25c0dbaa9d3daf2d0577"
+	apiKey: 'AIzaSyCzAcmL971bApw0_yvAIXFrApEJA0gy3rY',
+	authDomain: 'fir-photo-8d94a.firebaseapp.com',
+	databaseURL: 'https://fir-photo-8d94a.firebaseio.com',
+	projectId: 'fir-photo-8d94a',
+	storageBucket: 'fir-photo-8d94a.appspot.com',
+	messagingSenderId: '312521110742',
+	appId: '1:312521110742:web:db25c0dbaa9d3daf2d0577'
 };
 
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
-const projectStorage = firebase.storage()
-const projectStore = firebase.firestore()
+const projectStorage = firebase.storage();
+const projectFireStore = firebase.firestore();
 
-export{projectStorage, projectStore}
+export { projectStorage, projectFireStore };
