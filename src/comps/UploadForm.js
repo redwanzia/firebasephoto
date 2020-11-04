be41da65b19ede7cf6e58c4da4efa1a03bb15b29
redwan zia <redwan.zia@gmail.com>
2020-11-03 20:12:09 -0500

    added input handler function for image uplead

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const UploadForm = () => {
-  return (
-    <div>
-      <input type="file"/>
-    </div>
-  )
-}
+	const [ file, setFile ] = useState(null);
+	const [ error, setError ] = useState(null);
+	const types = [ 'image/png', 'image/jpeg' ];
 
-export default UploadForm
+	const changeHandler = (e) => {
+		const selected = e.target.files[0];
+		if (selected && types.includes(selected.type)) {
+			setFile(selected);
+			setError('');
+		} else {
+			setFile(null);
+			setError('Please select a valid image file (png or jpeg )');
+		}
+	};
+	return (
+		<div>
+			<input type='file' onChange={changeHandler} />
+			<div className='output'>
+				{error && <div className='error'>{error}</div>}
+				{file && <div>{file.name}</div>}
+			</div>
+		</div>
+	);
+};
+
+export default UploadForm;
